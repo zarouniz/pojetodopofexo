f4d80ec1887e2331d040bc918dd093941db30b05
Luiz Zarowny <luizmz96@gmail.com>
2025-01-11 19:53:37 -0300

    add myprofile

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/perfilLuiz.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/perfilLuiz.png")
   }
 }
